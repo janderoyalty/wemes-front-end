c28c4235446e94796df7a147b6276f1b9e8c0173
Jande Royalty <janderoyalty@gmail.com>
2022-07-30 03:56:33 -0500

    uninstalled drf-nested-routers

--- wemes/urls.py
+++ wemes/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework_nested import routers
 from wemes import views
 
 
